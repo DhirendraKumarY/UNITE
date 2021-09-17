d9eb38ae1eb532696472dfc95e33deba0bfa59c8
DhirendraKumarY <dhirendra.k@360logica.com>
2021-09-17 14:42:17 +0530

    I have add one demo to check on the same framework "google search

---
+++ FeatureFiles/SpecFlowFeature1.feature
@@ -0,0 +1,9 @@
+﻿Feature: SearchLetter
+	Search typed words on google chrome
+
+@mytag
+Scenario: Search words on google 
+	Given Launch Google Chrome 
+	And Navigate Google URL
+	When Typed for search letter 
+	Then Google has searced and quit
---
+++ StepDefination/SearchLetterSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Firefox;
+namespace SimpleUnitTestSpecFlow.StepDefinationFiles
+{
+    [Binding]
+    public class SearchLetterSteps
+    {
+
+        public IWebDriver driver;
+        [Given(@"Launch Google Chrome")]
+        public void GivenLaunchGoogleChrome()
+        {
+            Console.WriteLine("Simple Selenium Examples");
+            driver = new ChromeDriver();
+        }
+
+        [Given(@"Navigate Google URL")]
+        public void GivenNavigateGoogleURL()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+
+        [When(@"Typed for search letter")]
+        public void WhenTypedForSearchLetter()
+        {
+            driver.FindElement(By.Name("q")).SendKeys("Dhirendra Kumar Communication " + Keys.Enter);
+        }
+
+        [Then(@"Google has searced and quit")]
+        public void ThenGoogleHasSearcedAndQuit()
+        {
+            driver.Quit();
+        }
+    }
+}
